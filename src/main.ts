497f2f30a1ec31e39f890ae13bb62bd44437fdc3
Yousef Alamdarlou <yousefalamdarlou@gmail.com>
2024-01-06 15:58:34 +0330

    BearerAuth added to swagger

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
     .setTitle("test")
     .setDescription("The nest app API description")
     .setVersion("1.0")
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);

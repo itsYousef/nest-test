cea367de917be43f2ce94f2ae813fba058dcea6d
Yousef Alamdarlou <yousefalamdarlou@gmail.com>
2024-01-07 12:20:39 +0330

    refined

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { PrismaService } from '../prisma/prisma.service';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from '../auth/guard/roles.guard';
 
 @Module({
   controllers: [UserController],
